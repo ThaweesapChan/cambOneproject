30bc10a705283c509588ad8a67ca1ca5b603d08a
ทวีทรัพย์ ฉันทาสถานนท์ <thaweesap@192.168.1.52>
2023-01-21 00:33:26 +0700

    add file

---
+++ main.py
@@ -0,0 +1,21 @@
+import pyautogui
+import time
+import cv2
+import numpy as np
+
+screen_width, screen_height = pyautogui.size()
+buttonCheck 		 = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/buttonCheck.png")
+buttonCorrectChoice  = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/buttonCorrectChoice.png")
+buttonCycle 		 = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/ButtonCycle.png")
+buttonNext 			 = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/buttonNext.png")
+buttonNextAct 		 = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/buttonNextAct.png")
+buttonNextActMagenta = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/buttonNextActMagenta.png")
+buttonTriangle 		 = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/buttonTriangle.png")
+iconCorrect 		 = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/iconCorrect.png")
+pageLineGreenDark 	 = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/pageLineGreenDark.png")
+pageLineGreenNon 	 = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/pageLineGreenNon.png")
+pageLineMagentaDark  = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/pageLineMagentaDark.png")
+pageLineMagentaLight = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/pageLineMagentaLight.png")
+pageLineMagentaNon 	 = cv2.imread("/Users/thaweesap/CodingProject/cambOneProject/img/template/pageLineMagentaNon.png")
+
+
